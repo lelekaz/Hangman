f9631a90863cc22474c62c81949c9da3cc066605
lelekaz <57500218+lelekaz@users.noreply.github.com>
2020-09-15 21:45:43 -0400

    Commenting Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -1,3 +1,4 @@
+#This is the method that drives the whole program
 def Main():
     print("Welcome to Hangman!")
     correct_phrase = Get_Phrase()
@@ -10,12 +11,14 @@ def Main():
 
     Play_Hangman(correct_phrase, guess_phrase)
 
+#This proceeds to get the hangman phrase from Player 1
 def Get_Phrase():
     correct_phrase = ""
     while len(correct_phrase) == 0:
         correct_phrase = input("Player 1, enter a word/phrase for Player 2 to guess: ")
     return correct_phrase.upper()
 
+#This sets up the underscored hangman phrase that Player 2 sees and tries to guess from
 def Setup_Guess_Phrase(cp):
     guess_phrase = ""
     for x in range(len(cp)):
@@ -25,6 +28,7 @@ def Setup_Guess_Phrase(cp):
             guess_phrase += cp[x]
     return guess_phrase
 
+#This runs the part of the game where Player 2 guesses the letters and hangman phrase
 def Play_Hangman(cp, gp):
     correct_phrase = cp
     guess_phrase = gp
@@ -55,19 +59,20 @@ def Play_Hangman(cp, gp):
         if user_choice == "0":
             guess_temp = Guessing_Letter(correct_phrase, guess_phrase)
             if guess_phrase == guess_temp:
-                num_bad_guesses += 1
+                num_bad_guesses += 1 #If Player 2 guessed a wrong letter, another body part is drawn in the illustration
             guess_phrase = guess_temp
         elif user_choice == "1":
             got_it_right = Guessing_Phrase(correct_phrase)
             if got_it_right == True:
-                running = False
+                running = False #If Player 2 guessed the phrase correctly, the game ends and Player 2 wins
             else:
-                num_bad_guesses += 1            
+                num_bad_guesses += 1 #If Player 2 guessed the phrase incorrectly, another body part is drawn in the illustration
         elif user_choice == "2":
             running = False
         else:
             print("Input a valid response.")
 
+#This lets Player 2 guess a letter and edits the guess_phrase (underscored phrase) accordingly
 def Guessing_Letter(cp, gp):
     letter = ""
     while len(letter) != 1:
@@ -82,6 +87,7 @@ def Guessing_Letter(cp, gp):
 
     return guess_phrase
 
+#This lets Player 2 guess the hangman phrase, and returns a boolean value of whether they guessed correctly or not
 def Guessing_Phrase(cp):
     guess = input("Type in your guess for the hangman phrase: ")
     if guess.upper() == cp:
@@ -91,6 +97,8 @@ def Guessing_Phrase(cp):
         print("That's incorrect; try again.")
         return False
 
+#This is the printing of the Hangman illustration; it shows the different stages of the hangman body as the number
+#nbg gets larger
 def Print_Hangman_Body(nbg):
     print()
     print("          -------")
